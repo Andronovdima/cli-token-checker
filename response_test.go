be781f3051e9287d757409cac02f59422bb930a8
Andronovdima <hifromnorway@gmail.com>
2021-02-12 15:40:15 +0300

    add one test to response

--- response_test.go
+++ response_test.go
@@ -26,13 +26,24 @@ func TestResponse_Output(t *testing.T) {
 			expectedString: "client_id: idy\nclient_type: 11\n" +
 				"expires_in: 1880\nuser_id: 101\nusername: usk",
 		},
+		{
+			name: "Test Ok response with error",
+			resp: &Response{
+				SvcID:      2,
+				BodyLen:    18,
+				ReqID:      111,
+				ReturnCode: 2,
+				Err: "db error",
+			},
+			expectedString: "error: CUBE_OAUTH2_ERR_DB_ERROR\nmessage: db error",
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			str := tc.resp.Output()
 			if str != tc.expectedString {
-				t.Errorf("Response output Err\ngot :\n%swant:\n%s", str, tc.expectedString)
+				t.Errorf("Response output Err\ngot :\n%s\nwant:\n%s", str, tc.expectedString)
 			}
 			return
 		})
